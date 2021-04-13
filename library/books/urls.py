400cfc7c0743a8eb99a0cc456f370221ee83807f
Jacek Dembowiak <jacekdembowiak@gmail.com>
2021-04-13 22:03:38 +0200

    add book list endpoint with published date as filter param

--- library/books/urls.py
+++ library/books/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('<int:pk>', views.BookDetail.as_view(), name='detail'),
+    path('', views.BookList.as_view(), name="list"),
 ]
--- library/books/views.py
+++ library/books/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Book
 from .serializers import BookSerializer
@@ -22,3 +23,12 @@ class BookDetail(APIView):
         return Response(serializer.data)
 
 
+class BookList(generics.ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = Book.objects.all()
+        published_date = self.request.query_params.get('published_date')
+        if published_date is not None:
+            queryset = queryset.filter(published_date=published_date)
+        return queryset
