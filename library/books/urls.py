ded10412344c12e521c4ee6dbfa28f2c2c2655cf
Jacek Dembowiak <jacekdembowiak@gmail.com>
2021-04-16 19:19:32 +0200

    change url db/ to body base

--- library/books/urls.py
+++ library/books/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('books/<int:pk>', views.BookDetail.as_view(), name='detail'),
     path('books/', views.BookList.as_view(), name="list"),
-    path('db/<str:q>', views.LibrarySave.as_view(), name="save_books"),
+    path('db/', views.LibrarySave.as_view(), name="save_books"),
 ]
